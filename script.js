dad670f607c12c5af8f8a727e050ecebb6e8f449
Faith <fhgray96@gmail.com>
2022-05-21 22:10:09 -0500

    finished project

---
+++ script.js
@@ -0,0 +1,103 @@
+const track = document.querySelector('.carousel__track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel__button--right');
+const prevButton = document.querySelector('.carousel__button--left');
+const dotsNav = document.querySelector('.carousel__nav');
+const dots = Array.from(dotsNav.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+console.log(slideWidth);
+
+//put slides next to each other
+// slides[0].style.left = slideWidth * 0 + 'px';
+// slides[1].style.left = slideWidth * 1 + 'px';
+// slides[2].style.left = slideWidth * 2 + 'px';
+
+//breaking this out into a forEach loops lets you add any number of slides and have the width set itself. 
+//breaking this out into the variable setSlidePosition put into the loop allows for you to clearly state what's going on without putting in a note to explain.
+
+
+//functions
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + 'px';
+}
+slides.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+}
+
+const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+    if (targetIndex === 0) {
+        prevButton.classList.add('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    } else if (targetIndex === slides.length -1){
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.add('is-hidden');
+    } else {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    }
+
+}
+
+//when right button is clicked, move slides to right
+nextButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    moveToSlide(track, currentSlide, nextSlide);
+
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const nextDot = currentDot.nextElementSibling;
+    updateDots(currentDot, nextDot);
+
+    const nextIndex = slides.findIndex(slide => slide === nextSlide);
+    hideShowArrows(slides, prevButton, nextButton, nextIndex);
+
+    //move slide REFACTORED THIS ABOVE
+    // const amountToMove = nextSlide.style.left;
+    // track.style.transform = 'translateX(-' + amountToMove + ')';
+    // currentSlide.classList.remove('current-slide');
+    // nextSlide.classList.add('current-    slide');
+})
+
+//when I click left, move slides to the left
+prevButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    moveToSlide(track, currentSlide, prevSlide);
+
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const prevDot = currentDot.previousElementSibling;
+    updateDots(currentDot, prevDot);
+
+    const prevIndex = slides.findIndex(slide => slide === prevSlide);
+    hideShowArrows(slides, prevButton, nextButton, prevIndex);
+});
+
+//when click nav indicator, move to that slide
+dotsNav.addEventListener('click', e => {
+    //what indicator was clicked on
+    const targetDot = e.target.closest('button');
+
+    if (!targetDot) return;
+
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+    hideShowArrows(slides, prevButton, nextButton, targetIndex);
+
+    console.log(targetIndex);
+});
+
